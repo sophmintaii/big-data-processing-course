460d2ec36b846188f3b4c2fa05bdf26981764c99
Sofiia Myntiuk <sophmintaii@gmail.com>
2022-04-27 18:26:27 +0200

    added comments asa file and in README

--- select-data.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-docker exec -it node1 cqlsh -e "SELECT * FROM hw2_myntiuk.favorite_songs;"
-docker exec -it node1 cqlsh -e "SELECT * FROM hw2_myntiuk.favorite_movies;"
--- shutdown-cluster.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-docker kill node1
-docker kill node2
-docker kill node3
-docker network rm hw2_cassandra_installation
